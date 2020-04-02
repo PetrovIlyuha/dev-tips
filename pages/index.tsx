6ced8ed1d14ab5fb976f60e9d4dc250194075684
Ilya Petrov <dnamix1@gmail.com>
2020-04-02 15:48:03 +0300

    applying of the global styles (custom css variables) via styled-components ThemeProvider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { MainLayout } from '../components/layout/MainLayout';
 
 import styled from 'styled-components';
 
 const StyledHeader = styled.h1`
-  font-size: 2rem;
-  text-align: left;
-  padding: 16px 16px;
+  ${({ theme }) => `
+    font-size: 2rem;
+    text-align: left;
+    padding: ${theme['padding-small']} ${theme['padding-small']};
+  `}
 `;
 
 const Index = () => {
   return (
-    <div>
+    <MainLayout>
       <StyledHeader>The Index Page</StyledHeader>
-    </div>
+    </MainLayout>
   );
 };
 
