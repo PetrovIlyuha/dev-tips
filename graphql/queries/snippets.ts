b7c75149eee26a4932a2eb5cf74a360b63d6d118
Ilya Petrov <dnamix1@gmail.com>
2020-04-04 10:56:11 +0300

    multiple snippets query added

---
+++ graphql/queries/snippets.ts
@@ -0,0 +1,27 @@
+import gql from 'graphql-tag';
+
+export const snippetsGraphQL = gql`
+  query snippetsGraphQL($where: SnippetWhereInput) {
+    snippets(where: $where) {
+      id
+      status
+      title
+      content
+      description
+      recommendations
+      userLikes {
+        id
+        user
+      }
+      author
+      images {
+        id
+        fileName
+        height
+        width
+        size
+        handle
+      }
+    }
+  }
+`;
