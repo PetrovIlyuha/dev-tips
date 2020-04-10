76244ff4f5ab011546fd9532bb24b171fb8728d1
Ilya Petrov <dnamix1@gmail.com>
2020-04-10 16:50:34 +0300

    slight amendments in pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { MainLayout } from '../components/layout/MainLayout';
-import { SnippetsList, queryEnum } from '../components/SnippetsList';
-import styled from 'styled-components';
+import React from "react";
+import { MainLayout } from "../components/layout/MainLayout";
+import { SnippetsList, queryEnum } from "../components/SnippetsList";
+import styled from "styled-components";
 
 const StyledHeader = styled.h1`
   ${({ theme }) => `
     font-size: 2em;
     text-align: left;
-    padding: ${theme['padding-small']} ${theme['padding-small']};
+    padding: ${theme["padding-small"]} ${theme["padding-small"]};
   `}
 `;
 
