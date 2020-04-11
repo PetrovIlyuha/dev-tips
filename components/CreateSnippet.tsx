4c87b12ba6643f524e05df535a2f7589e24613b2
Ilya Petrov <dnamix1@gmail.com>
2020-04-11 13:31:21 +0300

    implemented custom controlled input-field generator

--- components/CreateSnippet.tsx
+++ components/CreateSnippet.tsx
@@ -1,13 +1,22 @@
 import { Form, Row, Col, Button } from "antd";
 import { submitForm } from "../utils/submitForm";
+import { GenerateInput } from "./GenerateFields";
 
 export const CreateSnippet = () => {
   const initiateCreateSnippet = () => {
     console.log("submitted form");
   };
-  const { values, handleSubmit } = submitForm({}, initiateCreateSnippet);
+  const { values, handleInputChange, handleSubmit } = submitForm(
+    { title: "" },
+    initiateCreateSnippet,
+  );
   return (
     <Form onSubmitCapture={handleSubmit}>
+      <GenerateInput
+        name="title"
+        value={values.title}
+        handleInputChange={handleInputChange}
+      />
       <Row>
         <Col span={16}></Col>
         <Col span={4}>
