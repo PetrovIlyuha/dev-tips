3f9e155e5c2bf7f922d9b25e6b092cff7b19f906
Ilya Petrov <dnamix1@gmail.com>
2020-04-11 13:32:24 +0300

    persistent state update function was created and exported from submitForm hook

--- utils/submitForm.ts
+++ utils/submitForm.ts
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import * as _ from "lodash";
 
 export const submitForm = (initialValues, callback) => {
   const [values, setValues] = useState(initialValues);
@@ -9,8 +10,18 @@ export const submitForm = (initialValues, callback) => {
     setValues(() => ({ ...initialValues }));
   };
 
+  const handleInputChange = event => {
+    event.persist();
+    setValues(prevInputs => {
+      const newInputs = _.cloneDeep(prevInputs);
+      _.set(newInputs, event.target.name, event.target.value);
+      return newInputs;
+    });
+  };
+
   return {
     values,
+    handleInputChange,
     handleSubmit,
   };
 };
