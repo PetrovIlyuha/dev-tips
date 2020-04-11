dc194a6f6f477156827ba41de08224c7bcd80a6f
Ilya Petrov <dnamix1@gmail.com>
2020-04-11 13:33:40 +0300

    custom fields generator components

---
+++ components/GenerateFields.tsx
@@ -0,0 +1,26 @@
+import { Row, Col, Input, Form } from "antd";
+
+type InputType = {
+  name: string;
+  value: string;
+  handleInputChange?: (event: any) => void;
+};
+
+export const GenerateInput = ({
+  name,
+  value,
+  handleInputChange,
+}: InputType) => (
+  <Row>
+    <Col span={12} offset={6}>
+      <Form.Item label={`${name}`}>
+        <Input
+          placeholder={`${name}`}
+          name={`${name}`}
+          value={value}
+          onChange={handleInputChange}
+        ></Input>
+      </Form.Item>
+    </Col>
+  </Row>
+);
